d05fe7b04654a264e9e6d070215ae64bce44929f
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-06-25 22:18:05 +0900

    add: mecab

--- sample.py
+++ sample.py
@@ -8,27 +8,27 @@ import glob
 import os
 import random
 
+import MeCab
 import ngram
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import (CountVectorizer,
+                                             HashingVectorizer,
+                                             TfidfVectorizer)
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score, classification_report, make_scorer
 from sklearn.model_selection import KFold
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.neural_network import MLPClassifier
 
 # In[35]:
+wakati = MeCab.Tagger(
+    "-r /home/linuxbrew/.linuxbrew/etc/mecabrc -d /home/linuxbrew/.linuxbrew/lib/mecab/dic/ipadic -O wakati")
 
 
 def tokenize(word):
-    word_list = []
-    for n in range(2, 4):
-        index = ngram.NGram(N=n)
-        for w in index.ngrams(index.pad(word)):
-            if w.find("$") != -1:
-                continue
-            word_list.append(w)
-    return word_list
+    tf = wakati.parse(word)
+    return tf.split()
 
 
 # In[36]:
@@ -58,7 +58,7 @@ compe_text = compe_df["text"].values.astype('U')
 # In[14]:
 
 
-vectorizer = TfidfVectorizer(analyzer=tokenize)
+vectorizer = HashingVectorizer(analyzer=tokenize)
 vectorizer.fit(train_text)
 X = vectorizer.transform(train_text)
 test_vec = vectorizer.transform(test_text)
@@ -77,13 +77,13 @@ kf = KFold(n_splits=3)
 for fold, (tr_idx, va_idx) in enumerate(kf.split(X, y)):
     x_train, y_train = X[tr_idx], y[tr_idx]
     x_valid, y_valid = X[va_idx], y[va_idx]
-    m = GaussianNB()
-    m.fit(x_train.toarray(), y_train)
+    m = MLPClassifier(max_iter=300, hidden_layer_sizes=(100,), verbose=10,)
+    m.fit(x_train, y_train)
     p = m.predict(x_valid)
     train_check[va_idx] += p
     models.append(m)
 
-print(accuracy_score(y, np.where(train_check < 0.5, 0, 1)))
+print("score", accuracy_score(y, np.where(train_check < 0.5, 0, 1)))
 
 # In[37]:
 
@@ -96,13 +96,14 @@ result = np.where(result < 0.5, 0, 1)
 # In[48]:
 
 
-print(accuracy_score(test_y, result))  # リーダーボードのスコア この値を毎回，discord上に載せてください．
+# リーダーボードのスコア この値を毎回，discord上に載せてください．
+print("score", accuracy_score(test_y, result))
 
 
 # In[41]:
 
 
-print(len(result))
+print("len", len(result))
 
 
 # In[45]:
@@ -112,7 +113,7 @@ c_result = np.zeros(len(compe_text), dtype=np.float64)
 c_result = np.array([model.predict(compe_vec)
                     for model in models]).mean(axis=0)
 c_result = np.where(c_result < 0.5, 0, 1)
-print(len(c_result))
+print("len", len(c_result))
 
 
 # In[46]:
