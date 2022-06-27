3dcc1752f512f8551bc10e4602419da3ab3b64fb
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-06-27 22:15:27 +0900

    fix: compe

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-from transformers import AutoTokenizer
+import MeCab
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from transformers import pipeline
 
-model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+# 感情分析
+nlp = pipeline("sentiment-analysis")
 
+# 推論
+print(nlp("I love you"))  # ポジティブ
+print(nlp("I hate you"))  # ネガティブ
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+train_df = pd.read_csv("./train.csv")
+test_df = pd.read_csv("./test.csv")
+compe_df = pd.read_csv("./compe.csv")
+
+train_text = train_df["text"].values.astype('U')
+test_text = test_df["text"].values.astype('U')
+y = train_df["label"].values.astype("int8")
+test_y = test_df["label"].values.astype("int8")
+compe_text = compe_df["text"].values.astype('U')
+
+y_pred = np.zeros(len(test_text))
+wakati = MeCab.Tagger(
+    "-r /home/linuxbrew/.linuxbrew/etc/mecabrc -d /home/linuxbrew/.linuxbrew/lib/mecab/dic/ipadic -O wakati")
+for i, text in enumerate(test_text):
+    if nlp(wakati.parse(text))[0]["label"] == "NEGATIVE":
+        y_pred[i] = 1
+
+print("score", accuracy_score(test_y, y_pred))
